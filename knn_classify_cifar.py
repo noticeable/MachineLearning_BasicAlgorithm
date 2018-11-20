10bc1f7615b1491b56e0f6acf6e1c45740471bd3
LeoLRH <3336987685@qq.com>
2018-11-20 10:34:26 +0800

    update readme

---
+++ knn_classify_cifar.py
@@ -0,0 +1,20 @@
+# # assume we have Xtr_rows, Ytr, Xte_rows, Yte as before
+# # recall Xtr_rows is 50,000 x 3072 matrix
+# Xval_rows = Xtr_rows[:1000, :]  # take first 1000 for validation
+# Yval = Ytr[:1000]
+# Xtr_rows = Xtr_rows[1000:, :]  # keep last 49,000 for train
+# Ytr = Ytr[1000:]
+#
+# # find hyperparameters that work best on the validation set
+# validation_accuracies = []
+# for k in [1, 3, 5, 10, 20, 50, 100]:
+#   # use a particular value of k and evaluation on validation data
+#   nn = NearestNeighbor()
+#   nn.train(Xtr_rows, Ytr)
+#   # here we assume a modified NearestNeighbor class that can take a k as input
+#   Yval_predict = nn.predict(Xval_rows, k=k)
+#   acc = np.mean(Yval_predict == Yval)
+#   print('accuracy: %f' % (acc,))
+#
+#   # keep track of what works on the validation set
+#   validation_accuracies.append((k, acc))
